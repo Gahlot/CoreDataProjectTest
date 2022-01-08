a11e274cd5ee59c2a4fd42a26159fbb467b7b413
Mahesh Gahlot <mahesh.gahlot@finoit.co.in>
2022-01-08 22:08:34 +0530

    Initial setup

---
+++ CoreDataProjectTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProjectTest
+//
+//  Created by macadmin on 08/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
